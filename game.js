40c5d0d5cb25b478e68ce09ad3c7e658c1426610
zad0809 <152491935+zad0809@users.noreply.github.com>
2026-04-14 21:57:30 +0800

    Update game.js

--- game.js
+++ game.js
@@ -360,7 +360,7 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowRight") rightPressed = true;
 
   // ⭐ 空白鍵發球
-  if (e.code === "Space" && waitingForServe && !countdownActive) {
+  if ((e.key === "q" || e.key === "Q") && waitingForServe && !countdownActive) {
     startBGM();
 
     for (const ball of balls) {
@@ -466,7 +466,7 @@ function drawText() {
     ctx.font = "20px Arial";
     ctx.fillStyle = "#facc15";
     ctx.fillText(
-      "Press Space to Serve",
+      "Press Q to Play",
       canvas.width / 2 - 110,
       canvas.height - 10
     );
